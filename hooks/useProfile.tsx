f40768d95d46d6de181fb01de5a2b61830d687ec
jaihindkushwah <jaihindkushwaha672001@gmail.com>
2024-09-25 18:26:10 +0530

    ci:created ci cd delpoyment script

--- hooks/useProfile.tsx
+++ hooks/useProfile.tsx
@@ -1,4 +1,3 @@
-// import { updateLoggedInState, updateProfile, updateToken } from "@/store/auth";
 import axios from "axios";
 import React, { useCallback } from "react";
 import { useDispatch } from "react-redux";
@@ -17,9 +16,6 @@ function useProfile() {
           "http://127.0.0.1:8080/api/v1/profile",
           config
         );
-        // dispatch(updateProfile(response.data));
-        // dispatch(updateLoggedInState(true));
-        // dispatch(updateToken(token));
         return response.data;
       } catch (error) {
         localStorage.removeItem("token");
